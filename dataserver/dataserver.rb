5e63dedd362b374e0666a26c2aaaa14a0cdd2dc2
Steeve McCauley <steeve@oneguycoding.com>
2016-02-29 12:08:13 -0500

    * dataserver now serves up content

--- dataserver/dataserver.rb
+++ dataserver/dataserver.rb
@@ -1,22 +1,115 @@
 #!/usr/bin/env ruby
 #
 
-require 'thread'
-require 'sinatra'
+require 'fileutils'
+require 'optparse'
+require 'logger'
 require 'json'
+require 'thread'
+require 'sinatra/base'
+require 'puma'
 
-$root=File.dirname(__FILE__)
-puts "root=#{$root}"
+ME=File.basename($0, ".rb")
+MD=File.expand_path(File.dirname(__FILE__))
+MFSD=ENV['MFSD']||File.join(MD,"data")
+TS=Time.new.strftime("%Y%m%d_%H%M")
+TMP=ENV['TMP'] || "/var/tmp"
+LOG_DIR=File.join(TMP, ME)
+NOW=Time.new.strftime("%Y%m")
+LOG_FILE="#{LOG_DIR}/#{ME}_#{NOW}.log"
 
-configure {
-	set :server, :puma
-	set :port, 1963
-	set :bind, '0.0.0.0'
-	set :root, File.dirname(__FILE__)
+DOC_ROOT=File.expand_path(File.join(MD, ".."))
+puts "root=#{DOC_ROOT}"
+
+class Logger
+    def die(msg)
+        self.error(msg)
+        exit 1
+    end
+end
+
+class String
+    def to_boolean
+        return self.downcase.eql?("true") ? true : false;
+    end
+end
+
+def set_logger(stream, level)
+    log = Logger.new(stream)
+    log.level = level
+    log.datetime_format = "%Y-%m-%d %H:%M:%S"
+    log.formatter = proc do |severity, datetime, progname, msg|
+        "#{severity} #{datetime}: #{msg}\n"
+    end
+    log
+end
+
+$log=set_logger(STDOUT, Logger::DEBUG)
+
+$log.info "Environment variable MFSD=#{MFSD}"
+$log.die "Mathflash server data directory not found: #{MFSD}" unless File.directory?(MFSD)
+
+$opts = {
+    :dir  => File.dirname(__FILE__),
+    :port => 1963,
+    :addr => '0.0.0.0',
+    :ssl  => false,
+    :pass => File.join(MFSD, "passwd.json"),
+    :log_file => nil,
+    :level => $log.level # Logger::DEBUG #INFO
 }
 
+optparser = OptionParser.new { |opts|
+    opts.banner = "#{ME}.rb [options]"
+
+    opts.on('-p', '--port PORT', Integer, "Server port, default=#{$opts[:port]}") { |port|
+        $opts[:port]=port
+    }
+
+    opts.on('-D', '--[no-]debug', "Control debug logging") { |debug|
+        $opts[:level] = debug ? Logger::DEBUG : Logger::INFO
+    }
+
+    opts.on('-l', '--logfile [FILE]', String, "Log file path, default=#{LOG_FILE}") { |log_file|
+        $opts[:log_file]=log_file||LOG_FILE
+    }
+
+    opts.on('-h', '--help', "Help") {
+        puts opts
+
+        puts "\npasswd data="+$opts[:pass]
+        puts
+
+        exit 0
+    }
+}
+optparser.parse!
+
+class MathFlashDataServer < Sinatra::Base
+
+    configure {
+        #set :root, ME
+        set :environment, :production
+        set :bind, $opts[:addr]
+        set :port, $opts[:port]
+        set :sessions, true
+		set :root, DOC_ROOT
+		set :public_folder, DOC_ROOT
+        set :show_exceptions, false
+        set :dump_errors, false
+        set :server, :puma
+    }
+
+    before do
+		Dir.chdir(DOC_ROOT)
+        #if request.request_method == 'GET' || request.request_method == 'POST'
+        #    response.headers["Access-Control-Allow-Origin"] = "*"
+        #    response.headers["Access-Control-Allow-Methods"] = "POST, GET"
+        #end
+    end
+
 helpers do
-	$mathflash_data = File.expand_path(File.join($root, "data/mathflash_data.json"))
+	$mathflash_data = File.join(MFSD, "mathflash_data.json")
 	$mutex=Mutex.new
 
 	def read_save_sync(file, opts={:save=>false})
@@ -76,7 +169,7 @@ helpers do
 end
 
 get '/' do
-	  "Hello World"
+	File.read("index.html")
 end
 
 # data format
@@ -171,3 +264,7 @@ end
 #		"Failed to parse mathflash data: #{$mathflash_data}"+e.to_s
 #	end
 #end
+	run!
+end
+
+
